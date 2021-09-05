e92ad3064ca0a8896811ffd2a814dfead2823e71
Peter <warrenp11@gmail.com>
2021-09-05 19:39:48 -0400

    window prompt to ask player's robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
